5bf740142b1c0b3d65f1f6902e5e9dd750e507e8
Michael Zuskin <90226705+Ursego@users.noreply.github.com>
2025-12-09 19:48:40 -0500

    Delete db directory

--- db/DbInfo.kt
+++
@@ -1,6 +0,0 @@
-package ca.intfast.iftimer.db
-
-object DbInfo {
-    const val NAME = "IntFast.db"
-    const val VERSION = 1 // increment if you change the database schema
-}
--- db/DbTable.kt
+++
@@ -1,5 +0,0 @@
-package ca.intfast.iftimer.db
-
-object DbTable {
-    const val CYCLE = "Cycle"
-}
